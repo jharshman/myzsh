e09466fb2f529aed77962b9cd5e45ba0e159a765
Josh Harshman <joshgreyhat@gmail.com>
2014-12-25 23:13:45 -0800

    zsh config push

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,8 @@
+# Colorize output, add file type indicator, and put sizes in human readable format
+alias ls='ls -GFh'
+
+# Same as above, but in long listing format
+alias ll='ls -GFhl'
+alias ri='ri -Tf ansi'
+alias grep='grep --color=auto'
+alias vtop='vtop --theme becca'
