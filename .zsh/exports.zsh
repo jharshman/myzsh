5b15336babb1cbf26ae7c053cac960ff3c0ef63d
Josh Harshman <joshgreyhat@gmail.com>
2015-01-05 21:36:14 -0800

    update to sources

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -16,8 +16,11 @@ export GREP_COLOR='3;33'
 # This resolves issues install the mysql, postgres, and other gems with native non universal binary extensions
 export ARCHFLAGS='-arch x86_64'
 
+# Export Path
+export PATH=/usr/bin/ruby:/home/voodoo/.gem/ruby/2.1.0/bin:${PATH}
+
 #export LESS='--ignore-case --raw-control-chars'
-export PAGER='most'
+export PAGER='less'
 
 export EDITOR=vi
 
